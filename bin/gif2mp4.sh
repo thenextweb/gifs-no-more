d4a6086eaa4956f916b71b23261406f6fcd342a6
Julio Foulquie <julio@thenextweb.com>
2016-06-16 10:40:09 +0200

    Fix issue on mp4 converter.

--- bin/gif2mp4.sh
+++ bin/gif2mp4.sh
@@ -21,5 +21,6 @@ OUTFILE="${INFILE%.gif}.mp4"
 # current directory
 OUTFILE="$(dirname -- "${OUTFILE}")/$(basename -- "${OUTFILE}")"
 
-# the scale bit is because x264 (yuv420p, to be exacts) needs image dimensions divisible by 2
-ffmpeg -i "${INFILE}" -c:v libx264 -an -movflags +faststart -vf 'scale=-2:ih' -pix_fmt yuv420p "${OUTFILE}"
+# the scale bit is because x264 (yuv420p, to be exacts) needs image dimensions divisible by 2, see http://stackoverflow.com/questions/20847674/ffmpeg-libx264-height-not-divisible-by-2#20848224
+# 'scale=-2:ih' will fail if both, width and height are odd
+ffmpeg -i "${INFILE}" -c:v libx264 -an -movflags +faststart -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' -pix_fmt yuv420p "${OUTFILE}"
