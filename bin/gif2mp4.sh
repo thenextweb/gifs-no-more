53bee9063e39dc6fd4258b87a7e5c7bd845b6916
Julio Foulquie <julio@thenextweb.com>
2021-09-17 10:57:59 -0300

    Don't overwrite existing mp4s

--- bin/gif2mp4.sh
+++ bin/gif2mp4.sh
@@ -23,4 +23,4 @@ OUTFILE="$(dirname -- "${OUTFILE}")/$(basename -- "${OUTFILE}")"
 
 # the scale bit is because x264 (yuv420p, to be exacts) needs image dimensions divisible by 2, see http://stackoverflow.com/questions/20847674/ffmpeg-libx264-height-not-divisible-by-2#20848224
 # 'scale=-2:ih' will fail if both, width and height are odd
-ffmpeg -i "${INFILE}" -c:v libx264 -an -movflags +faststart -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' -pix_fmt yuv420p "${OUTFILE}"
+ffmpeg -n -i "${INFILE}" -c:v libx264 -an -movflags +faststart -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' -pix_fmt yuv420p "${OUTFILE}"
